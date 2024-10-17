fd0a7d8b1f608f5bee78bcfa30f9601738c62372
eldahl <71466904+eldahl@users.noreply.github.com>
2024-10-17 14:10:42 +0200

    Made shader class and finished chapter 'Shaders'. Moving onwards to Textures

--- shader.cpp
+++ shader.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <sstream>
 
-Shader(const char *vertexPath, const char *fragmentPath) {
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
   std::string vertexCode;
   std::string fragmentCode;
 
@@ -40,22 +40,64 @@ Shader(const char *vertexPath, const char *fragmentPath) {
   vertex = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertex, 1, &vShaderCode, NULL);
   glCompileShader(vertex);
-  checkCompileErrors(vertex, "VERTEX");
+  _checkCompileErrors(vertex, "VERTEX");
 
   // Fragment
   fragment = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragment, 1, &fShaderCode, NULL);
   glCompileShader(fragment);
-  checkCompileErrors(fragment, "FRAGMENT");
+  _checkCompileErrors(fragment, "FRAGMENT");
 
   // Shader
   ID = glCreateProgram();
   glAttachShader(ID, vertex);
   glAttachShader(ID, fragment);
   glLinkProgram(ID);
-  checkCompileErrors(ID, "PROGRAM");
+  _checkCompileErrors(ID, "PROGRAM");
 
   // Clean up
   glDeleteShader(vertex);
   glDeleteShader(fragment);
+}
+
+// utility function for checking shader compilation/linking errors.
+// ------------------------------------------------------------------------
+void Shader::_checkCompileErrors(unsigned int shader, std::string type)
+{
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM")
+    {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+    else
+    {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+}
+
+void Shader::use() {
+  glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
--- shader.h
+++ shader.h
@@ -19,6 +19,9 @@ public:
   void setBool(const std::string &name, bool value) const;
   void setInt(const std::string &name, int value) const;
   void setFloat(const std::string &name, float value) const;
+
+private:
+  void _checkCompileErrors(unsigned int shader, std::string type);
 };
 
 #endif
