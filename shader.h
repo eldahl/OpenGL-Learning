2d15650624753a833bf7dab1e72be214ee9ba249
eldahl <71466904+eldahl@users.noreply.github.com>
2024-10-19 00:04:17 +0200

    Done with 'Coordinate Systems', next up Camera

--- shader.h
+++ shader.h
@@ -3,6 +3,8 @@
 
 #include <glad/glad.h>
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -19,6 +21,7 @@ public:
   void setBool(const std::string &name, bool value) const;
   void setInt(const std::string &name, int value) const;
   void setFloat(const std::string &name, float value) const;
+  void setMat4(const std::string &name, glm::mat4 value) const;
 
 private:
   void _checkCompileErrors(unsigned int shader, std::string type);
